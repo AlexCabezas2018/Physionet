a8197ebdc0792b5746ab373bce572796235d152d
alexcabezas <acabezas@nextdigital.es>
2020-02-27 14:26:55 +0100

    Naming refactoring

--- src/main/java/es/ucm/fdi/physionet/controller/RootController.java
+++ src/main/java/es/ucm/fdi/physionet/controller/RootController.java
@@ -1,8 +1,6 @@
 package es.ucm.fdi.physionet.controller;
 
-import es.ucm.fdi.physionet.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.support.CustomSQLErrorCodesTranslation;
 import org.springframework.stereotype.Controller;
 
 import org.apache.logging.log4j.LogManager;
@@ -29,40 +27,40 @@ public class RootController {
     public String menssageView(Model model) {
         log.debug("Hemos entrado en la vista de mensajes");
         model.addAttribute("userName", "Fernando Jiménez");
-        return "messages";
+        return "messages-view";
     }
 
     @GetMapping("/future-appointments-medic")
     public String futureAppointmentsMedic(Model model) {
         log.debug("Hemos entrado a la vista de citas para el día de hoy");
         model.addAttribute("patientUserName", "Elena Martinez");
-        return "medic-main-view";
+        return "medic-appointments";
     }
     
     @GetMapping("/historial")
     public String historialView(Model model) {
         log.debug("Hemos entrado a la ventana de histoarial de citas");
         model.addAttribute("patientUserName", "Elena Martinez");
-        return "historial";
+        return "patient-history";
     }
     
      @GetMapping("/citas-pendientes")
     public String citasView(Model model) {
         log.debug("Hemos entrado a la ventana de citas pendientes");
         model.addAttribute("patientUserName", "Elena Martinez");
-        return "citas-pendientes";
+        return "patient-appointments";
     }
 
     @GetMapping("/patient-profile")
     public String patientProfile() {
         log.debug("Hemos entrado en el perfil de un paciente");
-        return "perfil-pac";
+        return "patient-profile";
     }
 
     @GetMapping("/admin-pacient")
     public String adminPacientView() {
         log.debug("Hemos entrado en la vista de admin viendo el perfil de un paciente");
-        return "admin-pacient";
+        return "admin-patient-view";
     }
     @GetMapping("/admin-medic")
     public String adminMedicView() {
@@ -74,7 +72,7 @@ public class RootController {
     public String calendarView(Model model) {
         log.debug("Hemos entrado en la vista del calendario");
         model.addAttribute("missingDays", 3);
-        return "calendar";
+        return "absences-view";
     }
 
 }

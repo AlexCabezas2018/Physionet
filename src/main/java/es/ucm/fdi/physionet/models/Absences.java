77a48a79e78b15776f6e4e491165e018439c0588
FJavierLoz <60520365+FJavierLoz@users.noreply.github.com>
2020-03-01 20:21:39 +0100

    Update Absences.java

--- src/main/java/es/ucm/fdi/physionet/models/Absences.java
+++ src/main/java/es/ucm/fdi/physionet/models/Absences.java
@@ -1,4 +1,57 @@
 package es.ucm.fdi.physionet.models;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Absences {
+	
+	
+	public enum Reason {
+        SICKNESS,
+        PERSONALAFFAIRS,
+        HOLIDAYS,
+        CHILDBIRTH,
+        DEATHOFRElATIVE,
+        MEDICALSICKLEAVE,  
+        PREGNANCY,
+    }
+	
+	private Long id;
+	
+	private Date absenceDate;
+
+	private Reason reason;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	@ManyToOne
+	private User user;
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getAbsenceDate() {
+		return absenceDate;
+	}
+
+	public void setAbsenceDate(Date absenceDate) {
+		this.absenceDate = absenceDate;
+	}
+	public Reason getReason() {
+		return reason;
+	}
+
+	public void setReason(Reason reason) {
+		this.reason = reason;
+	}
 }

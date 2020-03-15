bae89e2ee641434fcac5e5ee88f55d60b19da706
FJavierLoz <60520365+FJavierLoz@users.noreply.github.com>
2020-03-15 20:42:41 +0100

    CRUD de Pacientes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,9 @@ INSERT INTO absence(id, date_from, date_to, details, reason, user_id) VALUES(1, 
 INSERT INTO user values(1, 1, 'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Martínez', 'patient');
 INSERT INTO user values(2, 1, 'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'DOCTOR', 'Martínez', 'doctor');
 INSERT INTO user values(3, 1, 'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'ADMIN', 'Martínez', 'admin');
+INSERT INTO user values(4, 1, 'Benito', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Pérez', 'patient');
+INSERT INTO user values(5, 1, 'Carlos', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Augusto', 'patient');
+INSERT INTO user values(6, 1, 'Daniel', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Pérez', 'patient');
+INSERT INTO user values(7, 1, 'Eustaquio', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Augusto', 'patient');
+INSERT INTO user values(8, 1, 'Fátima', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Pérez', 'patient');
+INSERT INTO user values(9, 1, 'Gabriela', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'PATIENT', 'Augusto', 'patient');

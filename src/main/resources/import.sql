cbd86ce09732a8f1c50970757349fbdc7c4fed54
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-03-16 20:05:23 +0100

    Testing messages functionality

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,4 +9,7 @@ INSERT INTO user values(1, 1, -1,'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95Jhc
 INSERT INTO user values(2, 1, 10,'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'DOCTOR', 'Martínez', 'doctor');
 INSERT INTO user values(3, 1, -1,'Arturo', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'ADMIN', 'Martínez', 'admin');
 
-INSERT INTO absence(id, date_from, date_to, details, reason, user_id) VALUES(1, '2020-03-07', '2020-03-10', 'detail_test_1', '2', 2);
+INSERT INTO absence(id, date_from, date_to, details, reason, user_id) VALUES(1, '2020-03-07', '2020-03-11', 'detail_test_1', '2', 2);
+
+/* Mensaje de un médico a un paciente */
+INSERT INTO message values(1, '2020-03-16', '2020-02-27', 'test', 1, 2);

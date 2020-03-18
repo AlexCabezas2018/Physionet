052e958c03b4f2104a7628e9e748538387d4fdd1
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-03-18 18:12:14 +0100

    Create class to centralize all messages sent to front

---
+++ src/main/java/es/ucm/fdi/physionet/model/util/ErrorMessages.java
@@ -0,0 +1,6 @@
+package es.ucm.fdi.physionet.model.util;
+
+public class ErrorMessages {
+    public static final String ABSENCE_TO_LONG = "¡No tienes días libres suficientes!";
+    public static final String ABSENCE_ADDED_SUCCESS = "¡Ausencia creada exitosamente!";
+}

af22a02078d2209193812c1dcbf0d77cda314e91
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-03-19 19:21:55 +0100

    Renamed messages class

--- src/main/java/es/ucm/fdi/physionet/model/util/ServerMessages.java
+++ src/main/java/es/ucm/fdi/physionet/model/util/ServerMessages.java
@@ -1,6 +1,6 @@
 package es.ucm.fdi.physionet.model.util;
 
-public class ErrorMessages {
+public class ServerMessages {
     public static final String ABSENCE_TO_LONG = "¡No tienes días libres suficientes!";
     public static final String ABSENCE_ADDED_SUCCESS = "¡Ausencia creada exitosamente!";
 }

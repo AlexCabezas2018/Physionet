1992a974f384c4b5a9b4a545f4f1398e85023b9f
JosMorcu <josmorcu@ucm.es>
2020-04-25 12:44:57 +0200

    Refactor validationNewUser and added validation to menssages.

--- src/main/resources/static/js/validationNewUser.js
+++ src/main/resources/static/js/validationNewUser.js
@@ -1,68 +1,10 @@
-function validateUsername(element, feedback, field) {
-    element.setCustomValidity(""); // limpia validaciones previas
-    if (element.value.length < 1) {
-        element.setCustomValidity("El "+ field +" debe estar relleno");
-        feedback.innerHTML ='<span class="badge badge-danger">El '+ field +' debe estar relleno</span>';
-        feedback.style.display = "block";
-    } else {
-        element.setCustomValidity("");
-        feedback.style.display = "none"
-    }
-}
-
-function validateText(element, feedback, field) {
-    element.setCustomValidity(""); // limpia validaciones previas
-    if (element.value.length < 1) {
-        element.setCustomValidity("El "+ field +" debe estar relleno");
-        feedback.innerHTML ='<span class="badge badge-danger">El '+ field +' debe estar relleno</span>';
-        feedback.style.display = "block";
-    } else if (!/^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ ]+$/.test(element.value)) {
-        element.setCustomValidity("El "+ field +" debe contener solo letras");
-        feedback.innerHTML = '<span class="badge badge-danger">El '+ field +' debe contener solo letras</span>';
-        feedback.style.display = "block";
-    } else {
-        element.setCustomValidity("");
-        feedback.style.display = "none"
-    }
-}
-
-function checkUsername() {
+function checkUsername(username) {
     // TODO revisar llamada allax (ahora mismo no funciona)
-    const username = document.getElementById('username');
-    let params = {username: username.value};
-    console.log(go("/admin/username?username="+ username.value, 'GET', params)
+    let params = {username: username};
+    (go("/admin/username?username="+ username, 'GET', params)
         .then(d => {console.log("usuario creado"); return true;})
         .catch(() => {console.log("usuario duplicado"); return false}));
 
-}
-
-function validatePass() {
-
-    const password = document.getElementById('password');
-    const pass2 = document.getElementById('pass2');
-    const feedbackPass = document.getElementById('feedbackPass');
-
-    if (pass2.value.localeCompare(password.value) === 0){
-        feedbackPass.style.display = "none";
-        return true;
-    } else {
-        feedbackPass.innerHTML = '<span class="badge badge-danger">Las dos contraseñas deben coincidir</span>';
-        feedbackPass.style.display = "block";
-        return false;
-    }
-    return false;
-}
-
-function validateForm(){
-    const feedbackUsername = document.getElementById('feedbackUsername');
-    let retUser = false;
-
-    if (!checkUsername()){
-        feedbackUsername.innerHTML = '<span class="badge badge-danger">El nombre de usuario ya existe</span>';
-        feedbackUsername.style.display = "block";
-    } else
-        retUser = true;
-    return validatePass() && retUser;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -74,16 +16,77 @@ document.addEventListener("DOMContentLoaded", () => {
     const username = document.getElementById('username');
     const name = document.getElementById('name');
     const surname = document.getElementById('surname');
+    const password = document.getElementById('password');
+    const pass2 = document.getElementById('pass2');
 
     const feedbackUsername = document.getElementById('feedbackUsername');
     const feedbackName = document.getElementById('feedbackName');
     const feedbackSurname = document.getElementById('feedbackSurname');
+    const feedbackPass = document.getElementById('feedbackPass');
 
-    username.oninput = () => validateUsername(username, feedbackUsername, "Nombre de usuario");
-    name.oninput = () => validateText(name, feedbackName, "Nombre");
-    surname.oninput = () => validateText(surname, feedbackSurname, "Apellido");
+    addInputEvents(username, feedbackUsername);
+    addInputEvents(name, feedbackName);
+    addInputEvents(surname, feedbackSurname);
+    addInputEvents(pass2, feedbackPass);
+    addInvalidEvents(username, name, surname, password, pass2, feedbackUsername, feedbackName, feedbackSurname, feedbackPass);
 
 });
+
+const addInputEvents = (element, feedback) => {
+    element.addEventListener('input', () => {
+        element.setCustomValidity('');
+        feedback.hasChildNodes() ? feedback.firstElementChild.style.display = "none" : "";
+        element.checkValidity();
+    });
+}
+
+const addInvalidEvents = (username, name, surname, password, pass2, feedbackUsername, feedbackName, feedbackSurname, feedbackPass) => {
+    username.addEventListener('invalid', () => {
+        if(username.value === '') {
+            username.setCustomValidity('El Nombre de Usuario debe estar relleno');
+            feedbackUsername.innerHTML = errorBadge('El Nombre de Usuario debe estar relleno');
+        } else {
+            checkUsername(username.value);
+            console.log("COMPROBANDO USER");
+        }
+    });
+
+    username.addEventListener('focusout', () => {
+        checkUsername(username.value);
+        console.log("COMPROBANDO USER");
+    });
+
+    name.addEventListener('invalid', () => {
+        if(name.value === '') {
+            name.setCustomValidity('El Nombre debe estar relleno');
+            feedbackName.innerHTML = errorBadge('El Nombre debe estar relleno');
+        } else {
+            name.setCustomValidity('El Nombre solo puede contener letras');
+            feedbackName.innerHTML = errorBadge('El Nombre solo puede contener letras');
+        }
+    });
+
+    surname.addEventListener('invalid', () => {
+        if(surname.value === '') {
+            surname.setCustomValidity('El Apellido debe estar relleno');
+            feedbackSurname.innerHTML = errorBadge('El Apellido debe estar relleno');
+        } else {
+            surname.setCustomValidity('El Apellido solo puede contener letras');
+            feedbackSurname.innerHTML = errorBadge('El Apellido solo puede contener letras');
+        }
+    });
+
+    pass2.addEventListener('focusout', () => {
+        if (pass2.value.localeCompare(password.value) !== 0){
+            pass2.setCustomValidity('Las contraseñas deben coincidir');
+            feedbackPass.innerHTML = errorBadge('Las contraseñas deben coincidir');
+        }
+    });
+}
+
+const errorBadge = (text) => {
+    return ('<span class="badge badge-danger">'+text+'</span>');
+}
 
 // envía json, espera json de vuelta; lanza error si status != 200
 function go(url, method, data = {}) {

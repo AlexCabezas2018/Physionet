98006d04920b945d471a8fd551155e5d211e32aa
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-05-03 11:10:11 +0200

    Fix html script loading in admin view

--- src/main/resources/static/js/adminUtils.js
+++ src/main/resources/static/js/adminUtils.js
@@ -1,37 +1,37 @@
 function sortListByName(){
-    let list, i , switching, b, shouldswitch, dir, switchCount=0 ;
+    let list, i , switching, b, shouldSwitch, dir, switchCount = 0;
     list = document.getElementById("patientList");
-    switching=true;
-    dir="asc";
-    while(switching){
-        switching=false;
-        b=list.getElementsByTagName("a");
-        for(i=0;i<(b.length-1);i++){
-            shouldswitch=false;
-            if(dir=="asc"){
+    switching = true;
+    dir = "asc";
+    while(switching) {
+        switching = false;
+        b = list.getElementsByTagName("a");
+        for(i = 0; i < b.length-1; i++) {
+            shouldSwitch = false;
+            if(dir === "asc") {
                 if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
                     shouldSwitch = true;
                     break;
                 }
             }
-            else if(dir="desc"){
+            else if(dir === "desc") {
                 if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-                    shouldSwitch= true;
+                    shouldSwitch = true;
                     break;
                 }
             }
         }
-        if(shouldSwitch){
+        if(shouldSwitch) {
             b[i].parentNode.insertBefore(b[i + 1], b[i]);
             switching=true;
-            switchcount++;
+            switchCount++;
         }
-        else{
-            if (switchcount == 0 && dir == "asc") {
+        else {
+            if (switchCount === 0 && dir === "asc") {
                 dir = "desc";
                 switching = true;
             }
         }
     }
-    
+
 }

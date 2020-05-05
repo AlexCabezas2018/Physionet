8d5db1bd2dd369c1eff4185a2c2e482494f5e1bb
FJavierLoz <60520365+FJavierLoz@users.noreply.github.com>
2020-05-05 10:02:59 +0200

    sorting admin list fixed

--- src/main/resources/static/js/adminUtils.js
+++ src/main/resources/static/js/adminUtils.js
@@ -1,6 +1,8 @@
 function sortListByName(){
-    let list, i , switching, b, shouldSwitch, dir, switchCount = 0;
-    list = document.getElementById("patientList");
+    let list,button, i , switching, b, shouldSwitch, dir, switchCount = 0;
+    list = document.querySelector('[id$="List"]');
+    button = document.getElementById("nameSortButton");
+    button.getElementsByTagName("span")[0].innerHTML = "↑";
     switching = true;
     dir = "asc";
     while(switching) {
@@ -22,16 +24,60 @@ function sortListByName(){
             }
         }
         if(shouldSwitch) {
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            b[i].parentNode.parentNode.insertBefore(b[i + 1].parentNode, b[i].parentNode);
             switching=true;
             switchCount++;
         }
         else {
             if (switchCount === 0 && dir === "asc") {
                 dir = "desc";
+                button.getElementsByTagName("span")[0].innerHTML = "↓";
                 switching = true;
             }
         }
     }
 
+}
+
+function sortListBySurname(){
+    let list,surnameList=[],button, i , switching, b, shouldSwitch, dir, switchCount = 0;
+    list = document.querySelector('[id$="List"]');
+    button = document.getElementById("surnameSortButton");
+    button.getElementsByTagName("span")[0].innerHTML = "↑";
+    switching = true;
+    dir = "asc";
+    while(switching) {
+        switching = false;
+        b = list.getElementsByTagName("a");
+        for(j = 0; j < b.length; j++){
+            surnameList[j] = b[j].innerHTML.split(" ");
+        }
+        for(i = 0; i < b.length-1; i++) {
+            shouldSwitch = false;
+            if(dir === "asc") {
+                if (surnameList[i][1].toLowerCase() > surnameList[i + 1][1].toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            else if(dir === "desc") {
+                if (surnameList[i][1].toLowerCase() < surnameList[i + 1][1].toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if(shouldSwitch) {
+            b[i].parentNode.parentNode.insertBefore(b[i + 1].parentNode, b[i].parentNode);
+            switching=true;
+            switchCount++;
+        }
+        else {
+            if (switchCount === 0 && dir === "asc") {
+                dir = "desc";
+                button.getElementsByTagName("span")[0].innerHTML = "↓";
+                switching = true;
+            }
+        }
+    }
 }
